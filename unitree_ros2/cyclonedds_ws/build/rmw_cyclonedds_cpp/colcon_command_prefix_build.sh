f7f12f4aac0eeb4e30b1db65dc7ad355ffc46ca2
Selina22 <selinaw187@gmail.com>
2025-10-12 01:28:48 -0400

    removed build and install

--- unitree_ros2/cyclonedds_ws/build/rmw_cyclonedds_cpp/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/unitree/unitree_ros2/cyclonedds_ws/install/cyclonedds/share/cyclonedds/package.sh"
--- unitree_ros2/cyclonedds_ws/build/unitree_api/unitree_api__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/unitree/unitree_ros2/cyclonedds_ws/build/unitree_api/unitree_api__py
-# Build directory: /home/unitree/unitree_ros2/cyclonedds_ws/build/unitree_api/unitree_api__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
